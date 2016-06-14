79d0b6eaea87a1e9ea1abc2fcb96d2fc8a104e49
Thomas Loy <ths.loy@googlemail.com>
2016-06-14 06:12:28 +0200

    Command interface

--- src/main/java/com/winthier/bookhead/BookHeadPlugin.java
+++ src/main/java/com/winthier/bookhead/BookHeadPlugin.java
@@ -2,9 +2,14 @@ package com.winthier.bookhead;
 
 import java.util.UUID;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEditBookEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -29,5 +34,23 @@ public class BookHeadPlugin extends JavaPlugin implements Listener {
         getServer().dispatchCommand(getServer().getConsoleSender(), cmd);
         event.getPlayer().sendMessage("" + ChatColor.DARK_AQUA + ChatColor.BOLD + "BookHead" + ChatColor.RESET + ChatColor.ITALIC + " Enjoy your custom player head!");
         event.setCancelled(true);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = sender instanceof Player ? (Player)sender : null;
+        if (player == null) return false;
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (item.getType() != Material.WRITTEN_BOOK) return false;
+        BookMeta meta = (BookMeta)item.getItemMeta();
+        if (meta.getPageCount() != 1) return false;
+        String page = meta.getPage(1);
+        if (!page.matches("[a-zA-Z0-9=]+")) return false;
+        String name = player.getName();
+        String cmd = String.format("give %s skull 1 3 {display:{Name:\"%s\"},SkullOwner:{Id:\"%s\",Properties:{textures:[{Value:\"%s\"}]}}}", name, name, UUID.randomUUID().toString(), page);
+        getLogger().info("Running command: " + cmd);
+        getServer().dispatchCommand(getServer().getConsoleSender(), cmd);
+        player.sendMessage("" + ChatColor.DARK_AQUA + ChatColor.BOLD + "BookHead" + ChatColor.RESET + ChatColor.ITALIC + " Enjoy your custom player head!");
+        return true;
     }
 }
